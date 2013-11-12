69c0a8c253785b7eefe8730d8c5c12e97cb95563
Louis Tiao <louistiao@gmail.com>
2013-11-13 00:27:41 +1100

    initial commit - list of all teams

---
+++ gamelogs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import requests, requests_cache, pprint, yaml
+
+requests_cache.install_cache('gamelog_cache')
+
+payload = {'LeagueID': '00'}    # The field LeagueID must match the regular expression '^\d{2}$'.
+r = requests.get('http://stats.nba.com/stats/commonteamyears', params=payload)
+raw_result = r.json()
+pprint.pprint(raw_result)
+#print raw_result[u'resultSets'][-1][u'headers']
