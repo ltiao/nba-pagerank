9a84c0b4174c96a9434432dce211e1257c786470
Louis Tiao <louistiao@gmail.com>
2013-11-13 00:47:30 +1100

    basic dict of id/name. modifying to comprehensive dict

--- gamelogs.py
+++ gamelogs.py
@@ -4,10 +4,11 @@ import requests, requests_cache, pprint, yaml
 
 requests_cache.install_cache('gamelog_cache')
 
-payload = {'LeagueID': '00'}    # The field LeagueID must match the regular expression '^\d{2}$'.
+with open("teams_config.yaml", 'r') as infile:
+    payload = yaml.load(infile)
+
 r = requests.get('http://stats.nba.com/stats/leaguedashteamstats', params=payload)
-print r.text
-exit(0)
 raw_result = r.json()
-pprint.pprint(raw_result)
-#print raw_result[u'resultSets'][-1][u'headers']
+#print raw_result
+#pprint.pprint(raw_result)
+print dict(row[:2] for row in raw_result[u'resultSets'][-1][u'rowSet'])
