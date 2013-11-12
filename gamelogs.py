208055d059d735fce1383e1f8686d7461ff9b30d
Louis Tiao <louistiao@gmail.com>
2013-11-13 00:58:45 +1100

    basic form

--- gamelogs.py
+++ gamelogs.py
@@ -9,7 +9,12 @@ with open("teams_config.yaml", 'r') as infile:
 
 r = requests.get('http://stats.nba.com/stats/leaguedashteamstats', params=payload)
 raw_result = r.json()
-header = raw_result[u'resultSets'][-1][u'headers'][1:]
-print header
-exit(0)
-print dict(row[:2] for row in raw_result[u'resultSets'][-1][u'rowSet'])
+headers = [h.lower() for h in raw_result[u'resultSets'][-1][u'headers'][1:]] # omit the 'TEAM_ID' header and convert everything to lowercase
+
+team_stats = {}
+
+for row in raw_result[u'resultSets'][-1][u'rowSet']:
+    team_id, row_data = row[0], row[1:] 
+    team_stats[team_id] = dict(zip(headers, row_data))
+
+pprint.pprint(team_stats)
