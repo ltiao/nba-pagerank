147ec438e0678c80ea08725d584d0baa8b4d692d
Louis Tiao <louistiao@gmail.com>
2013-11-13 21:45:38 +1100

    added first view

--- nba_pagerank/urls.py
+++ nba_pagerank/urls.py
@@ -5,8 +5,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'nba_pagerank.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'nba_pagerank.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
 )
