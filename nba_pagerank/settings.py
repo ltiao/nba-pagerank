cafe083affa538c44df0cad155f1afd8011e53ca
Louis Tiao <louistiao@gmail.com>
2013-11-14 02:11:36 +1100

    added redis cache

--- nba_pagerank/settings.py
+++ nba_pagerank/settings.py
@@ -102,3 +102,19 @@ STATICFILES_DIRS = (
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+
+import os
+import urlparse
+
+redis_url = urlparse.urlparse(os.environ.get('REDISTOGO_URL', 'redis://localhost:6959'))
+
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.RedisCache',
+        'LOCATION': '%s:%s' % (redis_url.hostname, redis_url.port),
+        'OPTIONS': {
+            'DB': 0,
+            'PASSWORD': redis_url.password,
+        }
+    }
+}
