aac56cf68f33588f5ea86ba6919c887b52407474
Louis Tiao <louistiao@gmail.com>
2013-11-20 02:05:00 +1100

    added datasource

--- nba_pagerank/datasets/data.py
+++ nba_pagerank/datasets/data.py
@@ -4,8 +4,8 @@ import datetime, redis, requests, requests_cache, pprint, yaml
 import networkx as nx
 
 redis = redis.from_url('redis://redistogo:d7a9d2fec3de08de3aa9a562cb34ad1e@beardfish.redistogo.com:10002/')
-#requests_cache.install_cache('gamelog_cache', backend='redis', expire_after=24*60*60, connection=redis) # 1 day
-requests_cache.install_cache('gamelog_cache', expire_after=24*60*60) # 1 day
+requests_cache.install_cache('gamelog_cache', backend='redis', expire_after=24*60*60, connection=redis) # 1 day
+# requests_cache.install_cache('gamelog_cache', expire_after=24*60*60) # 1 day
 
 def preprocess(raw_dict, identifier=0):
     """
@@ -52,35 +52,33 @@ def team_detail(team_id):
     r.raise_for_status()
     return r.json()[u'TeamDetails'][0][u'Details'][0]
 
-G = nx.MultiDiGraph()
-
-games = {}
-teams = all_team_stats()
-for team_id in teams:
-    detail = team_detail(team_id)
-    G.add_node(team_id, abbr=detail[u'Abbreviation'], city=detail[u'City'], nickname=detail[u'Nickname'])
-    team_games = team_gamelog(team_id, '2012-13')
-    for game_id in team_games:
-        if not game_id in games: games[game_id] = {}
-        if team_games[game_id].pop(u'wl') == 'W':
-            games[game_id][u'winner'] = team_id
-        else:
-            games[game_id][u'loser'] = team_id
-        matchup = team_games[game_id].pop(u'matchup')
-        if '@' in matchup: 
-            games[game_id][u'away'] = team_id
-        else: 
-            games[game_id][u'home'] = team_id
-        games[game_id][u'date'] = datetime.datetime.strptime(team_games[game_id].pop(u'game_date'), '%b %d, %Y').date()
-        games[game_id][u'minutes'] = team_games[game_id].pop(u'min')
-        games[game_id][team_id] = team_games[game_id]
+def graph(season='2013-14'):
+    G = nx.MultiDiGraph()
+    games = {}
+    teams = all_team_stats()
+    for team_id in teams:
+        detail = team_detail(team_id)
+        G.add_node(team_id, abbr=detail[u'Abbreviation'], city=detail[u'City'], nickname=detail[u'Nickname'])
+        team_games = team_gamelog(team_id, season)
+        for game_id in team_games:
+            if not game_id in games: games[game_id] = {}
+            if team_games[game_id].pop(u'wl') == 'W':
+                games[game_id][u'winner'] = team_id
+            else:
+                games[game_id][u'loser'] = team_id
+            matchup = team_games[game_id].pop(u'matchup')
+            if '@' in matchup: 
+                games[game_id][u'away'] = team_id
+            else: 
+                games[game_id][u'home'] = team_id
+            games[game_id][u'date'] = datetime.datetime.strptime(team_games[game_id].pop(u'game_date'), '%b %d, %Y').date()
+            games[game_id][u'minutes'] = team_games[game_id].pop(u'min')
+            games[game_id][team_id] = team_games[game_id]
         
-        if 'winner' in games[game_id] and 'loser' in games[game_id]:
-            G.add_edge(games[game_id]['loser'], games[game_id]['winner'], game_id, games[game_id])
+            if 'winner' in games[game_id] and 'loser' in games[game_id]:
+                G.add_edge(games[game_id]['loser'], games[game_id]['winner'], game_id, games[game_id])
+    return G
 
-M = nx.adjacency_matrix(G)
-
-exit(0)
-M = nx.pagerank_numpy(G, alpha=0.9)
-for team in sorted(M.keys(), key=lambda k: M[k], reverse=True):
-    print team, team_detail(team)[u'Abbreviation'], G.in_degree(team), G.out_degree(team), round(G.in_degree(team)/float(G.out_degree(team)+G.in_degree(team)), 3), M[team]
+# M = nx.pagerank_numpy(G, alpha=0.9)
+# for team in sorted(M.keys(), key=lambda k: M[k], reverse=True):
+#     print team, team_detail(team)[u'Abbreviation'], G.in_degree(team), G.out_degree(team), round(G.in_degree(team)/float(G.out_degree(team)+G.in_degree(team)), 3), M[team]
--- nba_pagerank/views.py
+++ nba_pagerank/views.py
@@ -1,9 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
+from datasets.data import graph
+import networkx as nx
 import numpy as np
 
 def home(request):
     return HttpResponse("Hello from django, try out <a href='/admin/'>/admin/</a>\n")
     
 def test(request):
-    return render_to_response('base.html', {'data': np.random.rand(10,5)})
+    G = graph('2013-14')
+    nodelist = sorted(G.nodes(), key = lambda a: G.node[a]['abbr'])
+    return render_to_response('base.html', {'data': np.array(nx.to_numpy_matrix(G, nodelist), dtype='int'), 'teams': [G.node[a]['abbr'] for a in nodelist]})
