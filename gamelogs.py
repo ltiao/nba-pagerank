8319a26ab1c8927f7ba4b61a28cda13dfbce5db1
Louis Tiao <louistiao@gmail.com>
2013-11-19 16:55:33 +1100

    numpy buildpack

--- gamelogs.py
+++ gamelogs.py
@@ -31,6 +31,37 @@ def team_gamelog(team_id, season, season_type='Regular Season'):
     r.raise_for_status()
     return preprocess(r.json(), identifier=1)
 
+def team_detail(team_id):
+    # [u'TeamDetails'][0][u'Details'][0]
+    r = requests.get('http://stats.nba.com/feeds/teams/profile/{team_id}_TeamProfile.js'.format(team_id=team_id))
+    r.raise_for_status()
+    return r.json()[u'TeamDetails'][0][u'Details'][0]
+
+import networkx as nx
+G = nx.MultiDiGraph()
+
+games = {}
+teams = all_team_stats()
+for team_id in teams:
+    detail = team_detail(team_id)
+    G.add_node(team_id, abbr=detail[u'Abbreviation'], city=detail[u'City'], nickname=detail[u'Nickname'])
+    team_games = team_gamelog(team_id, '2013-14')
+    for game_id in team_games:
+        won = int(team_games[game_id][u'wl'] == 'W')
+        if not game_id in games: games[game_id] = []
+        team_games[game_id][u'team_id'] = team_id
+        games[game_id].insert(won, team_games[game_id])
+        if len(games[game_id]) == 2: 
+            a = tuple(t[u'team_id'] for t in games[game_id])
+            G.add_edge(*a, key=game_id, weight=games[game_id][1][u'pts']-games[game_id][0][u'pts'])
+
+#print G[1610612737][1610612752]
+
+M = nx.google_matrix(G)
+print M
+
+exit(0)
+
 gamelog = {}
 teams = all_team_stats()
 for team_id in teams:
