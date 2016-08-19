cbc0b33cf73397038fc6ae92d7577ae66766295d
Louis Tiao <louistiao@gmail.com>
2016-08-20 00:34:34 +1000

    modifed project directory structure; no longer using multi-container pods

--- api/settings.py
+++ api/settings.py
@@ -1,5 +1,7 @@
 import os
 
+REQUESTS_CACHE_BACKEND = os.environ.get('REQUESTS_CACHE_BACKEND', 'sqlite')
+
 REDIS_HOST = os.environ['REDIS_MASTER_SERVICE_HOST'] \
           if os.environ.get('GET_HOSTS_FROM', '') == 'env' else 'redis-master'
 REDIS_PORT = 6379
--- build_run_all.sh
+++ build_run_all.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-docker build -t tiao/nba-rank:frontend-flask frontend/flask
-docker build -t tiao/nba-rank:frontend-nginx frontend/nginx
+docker build -t tiao/nba-rank:api-server api
+docker build -t tiao/nba-rank:frontend frontend
 docker build -t tiao/redis-commander redis-commander
 
-# docker push tiao/nba-rank:frontend-uwsgi 
-# docker push tiao/nba-rank:frontend-nginx 
+# docker push tiao/nba-rank:frontend-flask
+# docker push tiao/nba-rank:frontend-nginx
+# docker push tiao/redis-commander
 
 kubectl create -f artifacts
